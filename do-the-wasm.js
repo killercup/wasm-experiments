607c295dca7fb5f3601bf598c418418c26df9972
Pascal Hertleif <killercup@gmail.com>
2017-11-17 00:33:48 +0100

    fiddling

--- do-the-wasm.js
+++ do-the-wasm.js
@@ -9,30 +9,83 @@ const ensure = (test, error_essage) => !test && (error(error_essage), process.ex
 const [_node_path, script_name, wasm_file, fn_name, ...args] = process.argv.slice()
 ensure(wasm_file, `Usage: node ${script_name} <path-to.wasm> [args...]`)
 
-function copystr(memory, ptr) {
-  let result = "";
-  while (memory[ptr] != 0) {
-    result += String.fromCharCode(memory[ptr]);
-    ptr++;
+function copyCStr(memory, ptr) {
+  const collectCString = function* () { 
+    while (memory[ptr] !== 0) {
+      if (memory[ptr] === undefined) { throw new Error("Tried to read undef mem") }
+      yield memory[ptr]
+      ptr += 1
+    }
   }
-  return result;
+
+  const buffer_as_u8 = new Uint8Array(collectCString())
+  const buffer_from_node = Buffer.from(buffer_as_u8)
+  const buffer_as_utf8 = buffer_from_node.toString('utf8')
+  return buffer_as_utf8
+}
+
+/// Retrieve `[ptr, len]` from position `offset` in `memory`
+function extractSlice(memory, offset) {
+  const pointer_width = 4
+  
+  const iter_to_i32 = (iter) => {
+    const bytes = new Uint8Array(iter)
+    const view = new DataView(bytes.buffer)
+    return view.getUint32()
+  }
+
+  const getI32 = function* (ptr) {
+    for (let index = 0; index < pointer_width; index++) {
+      if (memory[ptr] === undefined) {
+        debugger
+        throw new Error(`Tried to read undef mem at ${ptr}`)
+      }
+      yield memory[ptr + index]
+    }
+  }
+
+  const ptr = iter_to_i32(getI32(offset/8))
+  const len = iter_to_i32(getI32(offset/8 + pointer_width))
+
+  return [ptr, len]
+}
+
+function getStr(memory, ptr, len) {
+  const getData = function* (ptr, len) {
+    for (let index = 0; index < len; index++) {
+      if (memory[ptr] === undefined) { throw new Error(`Tried to read undef mem at ${ptr}`) }
+      yield memory[ptr + index]
+    }
+  }
+  
+  const buffer_as_u8 = new Uint8Array(getData(ptr/8, len/8));
+  const buffer_from_node = Buffer.from(buffer_as_u8);
+  const buffer_as_utf8 = buffer_from_node.toString('utf8');
+  return buffer_as_utf8;
 }
 
 function newstring(alloc, memory, str) {
-  let len = str.length;
-  let ptr = alloc(len);
+  info(typeof str)
+  let string_buffer = Buffer.from(str, 'utf8')
+  let len = string_buffer.length
+  let ptr = alloc(len)
 
-  for (i=0; i < len; i++) {
-    memory[ptr+i] = str.charCodeAt(i);
+  for (i = 0; i < len; i++) {
+    memory[ptr+i] = string_buffer[i]
   }
 
-  return ptr;
+  return ptr
+}
+
+async function instantiate(imports = {}) {
+  const code = fs.readFileSync(wasm_file)
+  const wasm_module = await WebAssembly.compile(code.buffer)
+  const instance = await new WebAssembly.Instance(wasm_module, imports)
+  return instance
 }
 
 async function main(params) {
-  code = fs.readFileSync(wasm_file)
-  const wasm_module = await WebAssembly.compile(code.buffer)
-  const instance = await new WebAssembly.Instance(wasm_module, {})
+  const instance = await instantiate()
 
   if (!fn_name) {
     info(`WASM file contains these exports:`, Object.keys(instance.exports))
@@ -42,27 +95,41 @@ async function main(params) {
   info(`Running ${fn_name} with these arguments:`, args)
   ensure(instance.exports[fn_name], `No ${fn_name} in WASM exports :(`)
 
-  var buf = new Uint8Array(instance.exports.memory.buffer);
-  let wasm_call_res = 0;
-
-  if (fn_name == "hi") {
-    let arg = newstring(instance.exports.alloc, buf, args[0])
-    log({arg: arg})
-    wasm_call_res = instance.exports[fn_name](arg)
-  } else if (fn_name == "digest") {
-    let arg = newstring(instance.exports.alloc, buf, args[0])
-    log({arg: arg})
-    wasm_call_res = instance.exports[fn_name](arg)
-  } else {
-    wasm_call_res = instance.exports[fn_name](...args)
-  }
-  log(wasm_call_res)
-
-  if (fn_name == "time" || fn_name == "digest") {
-    let s = copystr(buf, wasm_call_res);
-    log(s)
+  const { memory, alloc } = instance.exports
+  var buf = new Uint8Array(memory.buffer)
+  let wasm_call_res = 0
+  let arg = null
+  
+  switch (fn_name) {
+    case "hi":
+    case "digest":
+      arg = newstring(alloc, buf, args[0])
+      log(fn_name, `alloc`, { arg })
+      wasm_call_res = instance.exports[fn_name](arg)
+      log(fn_name, copyCStr(buf, wasm_call_res))
+      break
+    case "time_str":
+      arg = alloc(8)
+      log(fn_name, `alloc`, { arg })
+      wasm_call_res = instance.exports[fn_name](arg)
+      ensure(wasm_call_res === undefined, `oh, ${fn_name} shouldn't return anything!`)
+      let [ptr, len] = extractSlice(buf, arg)
+      info(fn_name, `slice`, ptr, len)
+      let str = getStr(buf, ptr, len)
+      info(fn_name, `str`, str)
+      break
+    default:
+      wasm_call_res = instance.exports[fn_name](...args)
+      let s = copyCStr(buf, wasm_call_res);
+      log(fn_name, "cstr", s)
+      break
   }
 }
 
 main()
 .catch(err => { error(err); process.exit(2) })
+
+module.exports = {
+  instantiate,
+  log, info, error, ensure,
+}
