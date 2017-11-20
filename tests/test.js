6a54a41237f15dbf305a0b3781891fda7324ae52
Pascal Hertleif <killercup@gmail.com>
2017-11-20 21:45:44 +0100

    Simplify tests

--- tests/test.js
+++ tests/test.js
@@ -6,37 +6,33 @@ test.beforeEach(async (t) => {
   t.context.i = await instantiate("./hello-wasm.wasm");
 });
 
-test(`It's WASM time`, async (t) => {
+test(`It's WASM time`, (t) => {
   const time = wrap(t.context.i.exports, "time", [], "CStr");
   t.deepEqual(time(), "Es ist fünf vor Zwölf!!");
 });
 
-test(`2 + 2 = 42`, async (t) => {
+test(`2 + 2 = 42`, (t) => {
   const add = wrap(t.context.i.exports, "add", ["i32", "i32"], "i32");
   t.deepEqual(add(2, 2), 42);
   t.deepEqual(add(1.1, 1.1), 42);
 });
 
-test(`Hello Jan-Erik`, async (t) => {
-  const i = await instantiate("./hello-wasm.wasm");
+test(`Hello Jan-Erik`, (t) => {
   const hi = wrap(t.context.i.exports, "hi", ["CStr"], "CStr");
   t.deepEqual(hi("Jan-Erik"), "Jan-Erik");
 });
 
-test(`Hello UTF-8!`, async (t) => {
-  const i = await instantiate("./hello-wasm.wasm");
+test(`Hello UTF-8!`, (t) => {
   const hi = wrap(t.context.i.exports, "hi", ["CStr"], "CStr");
   t.deepEqual(hi("Grüß Gott!"), "Grüß Gott!");
 });
 
-test(`sha1`, async (t) => {
-  const i = await instantiate("./hello-wasm.wasm");
+test(`sha1`, (t) => {
   const digest = wrap(t.context.i.exports, "digest", ["CStr"], "CStr");
   t.deepEqual(digest("foobar"), "8843d7f92416211de9ebb963ff4ce28125932878");
 });
 
-test(`string slice`, async (t) => {
-  const i = await instantiate("./hello-wasm.wasm");
+test(`string slice`, (t) => {
   const time = wrap(t.context.i.exports, "time_str", [], "&str");
   t.deepEqual(time(), "Es ist fünf vor Zwölf!!!");
 });
