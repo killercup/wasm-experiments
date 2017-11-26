027e4521594801ef0acb2281a183d26825a5d0bd
Pascal Hertleif <killercup@gmail.com>
2017-11-26 20:47:38 +0100

    Asyncify file reading in `instantiate`

--- src/wasm.js
+++ src/wasm.js
@@ -1,11 +1,13 @@
 const fs = require("fs");
+const { promisify } = require("util");
+const readFile = promisify(fs.readFile);
 
 /**
  * @param {string} wasmFile
  * @param {object} imports
  */
 exports.instantiate = async (wasmFile, imports = {}) => {
-  const code = fs.readFileSync(wasmFile);
+  const code = await readFile(wasmFile);
   const wasmModule = await WebAssembly.compile(Buffer.from(code));
   const instance = await new WebAssembly.Instance(wasmModule, imports);
   return instance;
