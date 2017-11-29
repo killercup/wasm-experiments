cf8ba685495eda2987950ddb48b18352dcd6ba07
Sendil Kumar <sendilkumarn@live.com>
2017-11-29 14:07:49 +0100

    adding test cases for the example

--- examples/rust/test.js
+++ examples/rust/test.js
@@ -1,41 +1,41 @@
+const { test } = require("ava");
 const { instantiate } = require("../../src/wasm");
 const wrap = require("../../src/wrap");
 
-async function sampleTime() {
-  const context = await instantiate("./hello-wasm.wasm");
-  const time = wrap(context.exports, "time", [], "CStr");
-  time(); // "Es ist fünf vor Zwölf!!"
-}
+test.beforeEach(async (t) => {
+  t.context.i = await instantiate("examples/rust/demo.wasm");
+});
 
-async function sampleAdd() {
-  const context = await instantiate("./hello-wasm.wasm");
-  const add = wrap(context.exports, "add", ["i32", "i32"], "i32");
-  add(2, 2); //42
-  add(1.1, 1.1); //42
-}
+test(`It's WASM time`, (t) => {
+  const time = wrap(t.context.i.exports, "time", [], "CStr");
+  t.deepEqual(time(), "Es ist fünf vor Zwölf!!");
+});
 
-async function sampleEcho() {
-  const context = await instantiate("./hello-wasm.wasm");
-  const echo = wrap(context.exports, "echo_str", ["&str"], "&str");
-  echo("Banana"); // "Banana"
-  echo("Apple"); // "Apple"
-  echo(""); // ""
-  echo("L͞o̕r̕em͟ ip̕s͏u͢m");  // "L͞o̕r̕em͟ ip̕s͏u͢m"
-}
+test(`2 + 2 = 42`, (t) => {
+  const add = wrap(t.context.i.exports, "add", ["i32", "i32"], "i32");
+  t.deepEqual(add(2, 2), 42);
+  t.deepEqual(add(1.1, 1.1), 42);
+});
 
-async function sampleIsFancy() {
-  const context = await instantiate("./hello-wasm.wasm");
-  const isFancy = wrap(context.exports, "is_fancy", ["&str", "bool"], "bool");
-  isFancy("Banana", true); //true
-  isFancy("Apple", false); //false
-  isFancy("N/A", true); // false
-}
+test(`str echo`, (t) => {
+  const echo = wrap(t.context.i.exports, "echo_str", ["&str"], "&str");
+  t.deepEqual(echo("Banana"), "Banana");
+  t.deepEqual(echo("Apple"), "Apple");
+  t.deepEqual(echo(""), "");
+  t.deepEqual(echo("L͞o̕r̕em͟ ip̕s͏u͢m"), "L͞o̕r̕em͟ ip̕s͏u͢m");
+});
 
-async function sampleAlloc() {
-  const context = await instantiate("./hello-wasm.wasm");
+test(`boolean`, (t) => {
+  const isFancy = wrap(t.context.i.exports, "is_fancy", ["&str", "bool"], "bool");
+  t.deepEqual(isFancy("Banana", true), true);
+  t.deepEqual(isFancy("Apple", false), false);
+  t.deepEqual(isFancy("N/A", true), false);
+});
+
+test(`slice roundtrip`, (t) => {
   const { newSlice, extractSlice } = require("../../src/wasm-io");
-  const { memory, alloc } = context.exports;
+  const { memory, alloc } = t.context.i.exports;
   const ptr = newSlice(memory, alloc, new Uint8Array([1, 2, 3, 4]));
   const [ptr2, len] = extractSlice(memory, ptr);
-  // len is 4
-}
+  t.deepEqual(4, len);
+});
